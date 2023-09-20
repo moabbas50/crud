0c77dde6be56355f4631f20f4d53d2661460db18
moabbas50 <mohamed.abbass356@gmail.com>
2023-09-20 13:42:50 +0300

    update

--- js/app.js
+++ js/app.js
@@ -4,20 +4,52 @@ let count = document.querySelector("#count");
 let depart = document.querySelector("#depart");
 let cbtn = document.querySelectorAll(".btn");
 let tbody = document.querySelector("#tbody");
-
-let mood = "creat"
+let allspan = document.querySelectorAll(".invalid");
+let allspaninvaled = document.querySelectorAll(".invalid-number");
+let numbinputt =document.querySelectorAll(".numberin"); 
+let allinput = document.querySelectorAll("input");
+let mood = "creat";
 let indexid ;
 
 
 let products =[];
+  
+let validationState1 = true ;
+let validationState2 = true ;
+
+for(let i=0 ; i < allinput.length ; i++ ){
+    allinput[i].addEventListener("keyup",()=>{
+         if(allinput[i].value == "" ){
+    allspan[i].classList.remove("invalid");
+    allinput[i].classList.add("invalid-span"); 
+    validationState1 = false;
+         }else{
+            allspan[i].classList.add("invalid");
+            allinput[i].classList.remove("invalid-span");    
+            validationState1 = true;
+         }
+     })
+ }
+ 
+ for(let i=0 ; i < numbinputt.length ; i++ ){
+    numbinputt[i].addEventListener("keyup",()=>{
+         if(numbinputt[i].value < 0 ){
+           allspaninvaled[i].classList.remove("invalid-number");
+           numbinputt[i].classList.add("invalid-span"); 
+           validationState2 = false;
+         }else{
+            allspaninvaled[i].classList.add("invalid-number");
+            numbinputt[i].classList.remove("invalid-span");  
+            validationState2 = true;  
+         }
+     })
+ }
 if(localStorage.products!= null){
     products = JSON.parse(localStorage.products);
 }
-else{
+else{  
     products =[];   
 }
-
-
 
 for(i=0;i<allcost.length ;i++){
     allcost[i].addEventListener("keyup",()=>{
@@ -29,7 +61,6 @@ for(i=0;i<allcost.length ;i++){
         allcost[4].value = ((taxvlu +price ) -  discountvlu) ;
     });
 };
-
 let createproduct =()=>{
     let newproduct ={
         title : title.value ,
@@ -40,29 +71,22 @@ let createproduct =()=>{
         netprice : allcost[4].value ,
         department : depart.value
     }
-    if(mood == "creat"){
-        if(newproduct.count > 1){
-            for(let i=0; i < count.value; i++){
-                products.push(newproduct);
-            }
-           
-        }else{
+    if( validationState1 == true && validationState2 == true ){
+        if(mood == "creat"){
             products.push(newproduct);
         }
-    }else{
-        products[indexid] = newproduct ;
-        mood = "creat"
-        cbtn[0].innerHTML = "Add Product" ;
-        cbtn[0].classList.replace("btn-updat","btn") ;
-        count.classList.remove("hide");
-
+        else{
+            products[indexid] = newproduct ;
+            mood = "creat"
+            cbtn[0].innerHTML = "Add Product" ;
+            cbtn[0].classList.replace("btn-updat","btn") ;
+            count.classList.remove("hide");    
+        }   
+        clearinput();
+        renderData();
+        localStorage.setItem("products",JSON.stringify(products)); 
     }
-
     
-    clearinput();
-    renderData();
-    localStorage.setItem("products",JSON.stringify(products));
-
 }
 
 let clearinput = () => {
